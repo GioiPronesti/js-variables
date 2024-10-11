c0f1f48fb2975b7da39062d43bd196fe3edaf2e5
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-11 16:54:53 +0200

    string js exercize 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -11,6 +11,12 @@ console.log(myName);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+ let number = 0; 
+ let text = 'is';
+ let bool = false; 
+
+ console.log(number, text, bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
